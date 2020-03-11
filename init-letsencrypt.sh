c1be6bde848f3cea5cb037c4ef19a71d15f0fe04
Samuel Asvanyi <samoasvanyi@gmail.com>
2020-03-11 14:47:44 +0100

    Modify script to work with multiple domains

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,66 +1,79 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
-
-domains=(worklifebarometer.com www.worklifebarometer.com worklifebarometer.se www.worklifebarometer.se worklifebarometer.dk www.worklifebarometer.dk)
+domains=( worklifebarometer.se worklifebarometer.com worklifebarometer.dk) # subdomain www. is included
 rsa_key_size=4096
 data_path="./data/certbot"
 email="tech@worklifebarometer.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+domain_args="" # Additional arguments for certbot available at https://certbot.eff.org/docs/using.html#certbot-command-line-options
 
 if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
+    read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
+    if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
+        exit
+    fi
 fi
 
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
+    echo "### Downloading recommended TLS parameters ..."
+    mkdir -p "$data_path/conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
+    curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
+    echo
 fi
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
+for domain in "${domains[@]}"; do
+    for (( n=1; n<=2; n++ )); do
+        if [ $n -ge 2 ]; then
+            domain="www.$domain"
+        fi
+        echo "### Creating dummy certificate for [$domain]"
+        path="/etc/letsencrypt/live/$domain"
+        mkdir -p "$data_path/conf/live/$domain"
+        docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
     -subj '/CN=localhost'" certbot
-echo
+    done
+done
 
 
 echo "### Starting nginx ..."
 docker-compose up --force-recreate -d nginx
 echo
 
-echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
-
-
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
 for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
+    for (( n=1; n<=2; n++ )); do
+        if [ $n -ge 2 ]; then
+            domain="www.$domain"
+        fi
+        echo "### Deleting dummy certificate for [$domain]"
+        docker-compose run --rm --entrypoint "\
+  rm -Rf /etc/letsencrypt/live/$domain && \
+  rm -Rf /etc/letsencrypt/archive/$domain && \
+  rm -Rf /etc/letsencrypt/renewal/$domain.conf" certbot
+    done
 done
+
+
+#Join $domains to -d args
+echo "### Requesting Let's Encrypt certificate for [${domains[*]}]"
+for domain in "${domains[@]}"; do
+    for (( n=1; n<=2; n++ )); do
+        if [ $n -ge 2 ]; then
+            domain="www.$domain"
+        fi
+        domain_args="$domain_args -d $domain"
+    done
+done
+echo $domain_args
 
 # Select appropriate email arg
 case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
+    "") email_arg="--register-unsafely-without-email" ;;
+    *) email_arg="--email $email" ;;
 esac
 
 # Enable staging mode if needed
@@ -75,6 +88,7 @@ docker-compose run --rm --entrypoint "\
     --agree-tos \
     --force-renewal" certbot
 echo
+
 
 echo "### Reloading nginx ..."
 docker-compose exec nginx nginx -s reload
